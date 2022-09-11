f34820a54b7a2b8644373d50e4348f72d8c4a31e
Rikuya Chiba <rikuya.chiba@gmail.com>
2022-09-11 14:51:31 +0900

    Create master blade

--- src/resources/views/layouts/master.blade.php
+++ src/resources/views/layouts/master.blade.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- Stylesheet --}}
+    <link rel="stylesheet" href="../../css/angler.css">
+    {{-- Bootstrap CDN --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js">
     @yield('title')
 </head>
 <body>
